ea21f0d9d65b330a31f6423923a85dab4de736c7
Remy Sharp <remy@remysharp.com>
2016-01-27 22:16:11 +0000

    fix: support cli without arg to read pwd

--- cli/index.js
+++ cli/index.js
@@ -6,7 +6,12 @@ var fs = require('then-fs');
 
 var usage = 'Usage: snyk-resolve <package-dir>';
 var args = require('./args')(process.argv);
-var src = args._.shift();
+var src = args._.shift() || process.cwd();
+
+if (args.h || args.help) {
+  console.log(usage);
+  process.exit(0);
+}
 
 var echo = function (res) {
   console.log(JSON.stringify(res, '', 2));
