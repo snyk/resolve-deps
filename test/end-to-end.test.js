c82147297af07937cf6a169768f88d531bfb39be
Remy Sharp <remy@remysharp.com>
2016-01-27 14:14:48 +0000

    fix: deep scoped packages pluck fixed
    Plus tests

--- test/end-to-end.test.js
+++ test/end-to-end.test.js
@@ -1,24 +1,36 @@
 var test = require('tap-only');
 var lib = require('../lib');
+var tree = require('@remy/npm-tree');
 
 test('end to end (no deps)', function (t) {
   lib(__dirname + '/fixtures/pkg-undef-deps')
-  .then(lib.logicalTree)
   .then(function (res) {
     t.ok(!!res, 'we have a result for package without deps');
   })
-  .catch(t.fail)
+  .catch(t.threw)
   .then(t.end);
 });
 
 test('end to end (no deps but has node_modules)', function (t) {
   lib(__dirname + '/fixtures/pkg-undef-deps-with-modules', { dev: true })
-  .then(lib.logicalTree)
   .then(function (res) {
     t.equal(res.dependencies.debug.extraneous, undefined, 'debug is valid');
     t.equal(res.dependencies.undefsafe.extraneous, true, 'undefsafe is extraneous');
     t.ok(!!res, 'we have a result for package without deps');
   })
-  .catch(t.fail)
+  .catch(t.threw)
+  .then(t.end);
+});
+
+test('end to end (this package)', function (t) {
+  lib(__dirname + '/../', { dev: true })
+  .then(function (res) {
+    var fixtures = res.dependencies['snyk-resolve-deps-fixtures'];
+    t.ok(fixtures, 'has the fixtures dep');
+    t.equal(fixtures.dependencies['@remy/npm-tree'].name, '@remy/npm-tree', 'has npm-tree');
+    t.equal(fixtures.dependencies['@remy/vuln-test'].name, '@remy/vuln-test', 'has vuln-test');
+    t.equal(res.dependencies['snyk-resolve-deps-fixtures'].dependencies.undefsafe.extraneous, true, 'is extraneous');
+  })
+  .catch(t.threw)
   .then(t.end);
 });
--- test/pluck.test.js
+++ test/pluck.test.js
@@ -1,37 +1,29 @@
 var test = require('tap-only');
 var pluck = require('../lib/pluck');
-var resolveTree = require('../lib/');
 var path = require('path');
 
-test.only('pluck (with all modules)', function (t) {
+test('pluck (with all modules)', function (t) {
   var res = require('./fixtures/walk.json');
-  // var fixture = path.resolve(__dirname, '..');
+  var plucked = null;
+  var name = 'lodash';
+  plucked = pluck(res, ['snyk-resolve-deps', 'lodash'], name, '*');
+  t.equal(plucked.name, name, 'found lodash in direct dep');
+  plucked = pluck(res, ['snyk-resolve-deps','snyk','inquirer', 'lodash'], name, '*');
+  t.equal(plucked.name, name, 'found in deduped path');
 
-  // resolveTree(fixture).then(function (res) {
-    var plucked = null;
-    var name = 'lodash';
-
-    plucked = pluck(res, ['snyk-resolve-deps'], name, '*');
-    t.equal(plucked.name, name, 'found lodash in direct dep');
-    plucked = pluck(res, ['snyk-resolve-deps','snyk','inquirer'], name, '*');
-    t.equal(plucked.name, name, 'found in deduped path');
-
-    plucked = pluck(res, ['__noop__','inquirer'], name, '*');
-    t.equal(plucked, false, 'not found');
+  plucked = pluck(res, ['__noop__','inquirer'], name, '*');
+  t.equal(plucked, false, 'not found');
 
   t.end();
-
 });
 
-test('pluck (search for scoped)', function (t) {
-  var fixture = path.resolve(__dirname, '..');
+test('pluck (try github as version)', function (t) {
+  var res = require(path.resolve(__dirname, '..', 'node_modules', 'snyk-resolve-deps-fixtures', 'jsbin-file-tree.json'));
+  var plucked = null;
+  var name = 'memcached';
 
-  resolveTree(fixture).then(function (res) {
-    var plucked = null;
-    var name = '@remy/vuln-test';
-
-    plucked = pluck(res, ['snyk-resolve-deps', 'snyk-resolve-deps-fixtures'], name, '*');
-    t.ok(!!plucked.length, 'found ' + name);
-  }).catch(t.fail).then(t.end);
+  plucked = pluck(res, ['jsbin'], name, '*');
+  t.notEqual(plucked, false, 'memcached found');
+  t.end();
 
 });
