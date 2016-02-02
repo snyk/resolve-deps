8ba7a29b1e33a6c5b1c51f709d5e18d060748ceb
Remy Sharp <remy@remysharp.com>
2016-02-02 15:15:07 +0000

    fix: filtering on the cli (helper) tool

--- cli/args.js
+++ cli/args.js
@@ -1,18 +1,30 @@
 module.exports = args;
 
-var alias = { d: 'dev' };
+var alias = { d: 'dev', c: 'count' };
+var strings = ['count'];
+var taken = [];
 
 function args(processargv) {
-  return processargv.slice(2).reduce(function reduce(acc, arg) {
+  return processargv.slice(2).reduce(function reduce(acc, arg, i, all) {
+    if (taken.indexOf(i) !== -1) {
+      return acc;
+    }
+
     if (arg.indexOf('-') === 0) {
       arg = arg.slice(1);
 
       if (alias[arg] !== undefined) {
-        acc[alias[arg]] = true;
-      } else if (arg.indexOf('-') === 0) {
-        acc[arg.slice(1)] = true;
-      } else {
+        arg = '-' + alias[arg];
+      }
+
+      if (arg.indexOf('-') === 0) {
+        arg = arg.slice(1);
         acc[arg] = true;
+
+        if (strings.indexOf(arg) !== -1) {
+          acc[arg] = all[i + 1];
+          taken.push(i + 1);
+        }
       }
     } else {
       acc._.push(arg);
--- cli/index.js
+++ cli/index.js
@@ -3,6 +3,9 @@
 var resolveTree = require('../lib/');
 var tree = require('@remy/npm-tree');
 var fs = require('then-fs');
+var toObject = require('snyk-module');
+var walk = require('../lib/walk');
+var semver = require('semver');
 
 var usage = 'Usage: snyk-resolve <package-dir>';
 var args = require('./args')(process.argv);
@@ -12,6 +15,7 @@ if (args.h || args.help) {
   console.log(usage);
   process.exit(0);
 }
+
 
 var echo = function (res) {
   console.log(JSON.stringify(res, '', 2));
@@ -30,6 +34,23 @@ Promise.resolve().then(function () {
       .then(function (res) {
         if (args.json) {
           return echo(res);
+        }
+
+        if (args.count) {
+          var match = toObject(args.count);
+          var count = [];
+          walk(res, function (dep) {
+            if (dep.name === match.name) {
+              if (semver.satisfies(dep.version, match.version)) {
+                count.push(dep);
+              }
+            }
+          });
+          console.log('%s %s@%s', count.length, match.name, match.version);
+          count.forEach(function (dep) {
+            console.log(' - %s - %s', dep.full, dep.from.join(' > '));
+          });
+          return;
         }
 
         console.log(tree(res));
