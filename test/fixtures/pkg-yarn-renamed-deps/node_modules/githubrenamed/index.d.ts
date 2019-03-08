ce8958ea59a8ba3bff1cae0a96829b4ac419b5f2
Konstantin Yegupov <konstantin@snyk.io>
2019-03-08 17:59:41 +0000

    Remove unnecessary files, ignore the lock

--- test/fixtures/pkg-yarn-renamed-deps/node_modules/githubrenamed/index.d.ts
+++
@@ -1,7 +0,0 @@
-// Definitions by: Zlatko Andonovski, Andrew Yang, Chandler Fang and Zac Xu
-
-declare function leftPad(str: string|number, len: number, ch?: string|number): string;
-
-declare namespace leftPad { }
-
-export = leftPad;
